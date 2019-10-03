dc3ff3e2ebf4f472187856121f2ca4b1c79f2321
adilkhan <adilkhan@ucsb.edu>
2019-10-02 18:18:58 -0700

    added an obstacle

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ var gravityfunction = function (){
 var obstacles = [];
 
 obstacles[0] = {
-    x: window.width,
+    x: canvas.width,
     y: 280
 };
 
@@ -38,21 +38,21 @@ function getDistance(x1,y1,x2,y2){
     let xDifference  = x2-x1
     let yDifference = y2-y1
     return Math.sqrt(Math.pow(xDifference,2) + Math.pow(yDifference,2))
-}
+};
+obstacleX = 925;
+obstacleY = 200
 
 function draw(){
 
-    c.clearRect(0,0,canvas.width,canvas.height)
-    // for(let i=0; i<1; i++){
-    //     c.drawImage(obstacle,obstacles.x,obstacles.y, 50, 50);
-    //     obstacles.x;
-    //     console.log(obstacles.x -= 10);
-    // };
+    c.clearRect(0,0,canvas.width,canvas.height);
+
     c.drawImage(bg,0,0,canvas.width, canvas.height);
+    c.drawImage(obstacle,obstacleX,360, 50, 50);
+    obstacleX -= 3;
     // c.drawImage(playerLeftImg, 20, 20 )
     c.drawImage(playerRightImg, marioX ,marioY);
     // c.drawImage(obstacle,280, 255, 50,50);
-    console.log(getDistance(925,200,marioX,marioY))
+    // console.log(getDistance(,200,marioX,marioY))
 
     requestAnimationFrame(draw);  
     gravityfunction();
